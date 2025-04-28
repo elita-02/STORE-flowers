4598f821dd5ac6d891d328f6f5d2e2bc12592051
elita-02 <jyldyzbekovna04@outlook.com>
2025-04-28 11:35:47 +0600

    fix fix

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,46 +1,6 @@
-// import React from 'react';
-// import "./Wishlist.scss";
-// import WishCard from '../../Components/WishCard/WishCard';
-// import { useSelector, useDispatch } from "react-redux";
-// import { removeWish } from '../../redux/wish/wishSlice';
-
-// function Wishlist() {
-//   const { items } = useSelector((state) => state.wishlist);
-//   const dispatch = useDispatch();
-
-//   const handleRemoveItem = (itemId) => {
-//     dispatch(removeWish(itemId));
-//   };
-
-//   if (items.length === 0) {
-//     return <div className="wishlist-empty"> У вас нет избранных товаров</div>;
-//   }
-
-//   return (
-//     <div className="wishlist container">
-//       <div className="wishlist-header">
-//         <p>Список желаемого ({items.length})</p>
-//       </div>
-      
-//       <div className="items-list">
-//         {items.map((item) => (
-//           <WishCard 
-//             key={item.id} 
-//             data={item}
-//             onRemove={handleRemoveItem} 
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Wishlist;
-
-
 import React from 'react';
 import "./Wishlist.scss";
-import WishCard from '../../Components/WishCard/WishCard';
+import WishCard from '../../Components/wishCard/WishCard';
 import { useSelector, useDispatch } from "react-redux";
 import { removeWish } from '../../redux/wish/wishSlice';
 
